57d89fdb9a56cc5a832eb66b20bcd7bdee347ade
Oleg <oleg.volobuev@scrile.com>
2022-07-18 13:50:10 +0700

    adds code for delete.js

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const errorMessage = 'FS operation failed';
+
+const dirPath = path.join(__dirname, 'files');
+const fileToRemove = path.join(dirPath, 'fileToRemove.txt');
+
 export const remove = async () => {
-    // Write your code here 
+  await fs.rm(fileToRemove, {}, (err) => {
+    if err throw Error(errorMessage);
+  })
 };
