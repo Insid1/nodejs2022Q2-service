fe1dfc8525a190625ba13e0c536ad1fd839e6099
Oleg <oleg.volobuev@scrile.com>
2022-07-18 18:12:04 +0700

    bug fixes in list.js adds code in read.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -6,7 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 const errorMessage = 'FS operation failed';
 
-const dirPath = path.join(__dirname, 'files1');
+const dirPath = path.join(__dirname, 'files');
 const fileToRemove = path.join(dirPath, 'fileToRemove.txt');
 
 export const list = async () => {
--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,18 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const errorMessage = 'FS operation failed';
+
+const dirPath = path.join(__dirname, 'files');
+const fileToRead = path.join(dirPath, 'fileToRead.txt');
+
 export const read = async () => {
-    // Write your code here 
+  const data = await fs.readFile(fileToRead, (err, data) => {
+    if (err) throw Error(errorMessage);
+    console.log(data.toString());
+  })
 };
+read();
