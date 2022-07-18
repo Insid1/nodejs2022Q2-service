e795379d578615e99f756ce0a96d1b483d411c5d
Oleg <oleg.volobuev@scrile.com>
2022-07-18 18:47:58 +0700

    adds code to env.js

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,3 +1,14 @@
-export const parseEnv = () => {
-    // Write your code here 
+// const initialText = 'RSS_';
+const initialText = 'CH';
+
+export const parseEnv = async () => {
+  Object
+    .entries(process.env)
+    .forEach(([key, value]) => {
+      if (key.startsWith(initialText)) {
+        console.log(`${key} = ${value};`)
+      }
+    })
 };
+
+parseEnv()
