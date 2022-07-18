0e2332dad4222b7c2c3747f92e96b1c74a561556
Oleg <oleg.volobuev@scrile.com>
2022-07-18 17:36:07 +0700

    adds code for list.js

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const errorMessage = 'FS operation failed';
+
+const dirPath = path.join(__dirname, 'files1');
+const fileToRemove = path.join(dirPath, 'fileToRemove.txt');
+
 export const list = async () => {
-    // Write your code here 
+  await fs.readdir(dirPath, (err, files) => {
+    if (err) throw Error(err);
+    files.forEach((fileName) => {
+      console.log(fileName);
+    })
+  })
 };
+
+list();
