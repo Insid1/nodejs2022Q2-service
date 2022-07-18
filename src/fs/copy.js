7297b6e23e17d54bc21acc2f8043b19b4ed0290d
Oleg <oleg.volobuev@scrile.com>
2022-07-18 13:21:00 +0700

    adds code for copy.js and create.js

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,44 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const errorMessage = 'FS operation failed';
+
+const dirPath = path.join(__dirname, 'files_copy');
+const folderToCopy = path.join(__dirname, 'files');
+
 export const copy = async () => {
-    // Write your code here 
+  // check if folder files  exist if don\'t throw err
+  await fs.access(folderToCopy, fs.constants.F_OK, (err) => {
+    if (err) {
+      throw Error(errorMessage);
+    }
+  })
+  // check if files_copy exists if exists throw err
+  await fs.access(dirPath, fs.constants.F_OK, (err) => {
+    if (err) {
+      console.log('folder files_copy doesn\'t exist shall create it');
+    } else {
+      throw Error(errorMessage);
+    }
+  })
+  // copy file from one folder to another
+  await fs.readdir(folderToCopy, (err, files) => {
+    // create directory
+    fs.mkdir(dirPath, {}, (err) => {
+      console.log(err);
+    });
+    // create loop to get filenames inside the directory
+    files.forEach((fileName) => {
+      // generate route for file to copy
+      const fileToCopyRoute = path.join(folderToCopy, fileName);
+      // copy files to created directory
+      fs.copyFile(fileToCopyRoute, `${dirPath}/${fileName}`, 0, (err) => {
+        if (err) throw Error(err);
+      });
+    });
+  });
+
 };
--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,12 @@
+import fs from 'node:fs/promises';
+
+const fileName = 'fresh.txt';
+const fileData = 'I am fresh and young';
+
 export const create = async () => {
-    // Write your code here 
+  try {
+    await fs.writeFile(fileName, fileData, { flag: 'wx' }); // option's flag 'wx' stands for failing operation if path exists
+  } catch {
+    throw Error('FS operation failed')
+  }
 };
