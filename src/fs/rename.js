19fb057be539e9275aea7a0f7160703569a30f8c
Oleg <oleg.volobuev@scrile.com>
2022-07-18 13:36:41 +0700

    adds code for rename.js

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const errorMessage = 'FS operation failed';
+
+const dirPath = path.join(__dirname, 'files');
+const fileToRename = path.join(dirPath, 'wrongFilename.txt');
+const renameText = 'properFilename.md';
+
 export const rename = async () => {
-    // Write your code here 
+    // check access to wrongFileName (err DONT exist throw err)
+    await fs.access(fileToRename, fs.constants.F_OK, (err) => {
+        if (err) throw Error(errorMessage);
+    })
+    // check access to properfileName (if exists throw err)
+    await fs.access(path.join(dirPath, renameText), fs.constants.F_OK, (err) => {
+        if (!err) throw Error(errorMessage);
+    })
+    // rename the file
+    await fs.rename(fileToRename, renameText, (err) => {
+        if (err) throw Error(err);
+        console.log('renaming successfully initialized!');
+    })
+
 };
+
+rename();
