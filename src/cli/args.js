1fe6e0049ff7fdb9c74ca794b4a84141e5b7dc61
Oleg <oleg.volobuev@scrile.com>
2022-07-19 21:18:16 +0700

    adds code to args.js

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,14 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const exactCMDArgument = process.argv[2];
+  const arrFromArg = exactCMDArgument.split(' ');
+  let result = '';
+
+  for (let i = 0; i < arrFromArg.length; i += 2) {
+    const currVal = arrFromArg[i];
+    console.log(i)
+    if (currVal.startsWith('--')) {
+      result += `${currVal.slice(2)} is ${arrFromArg[i + 1]} `;
+    }
+  }
+  console.log(result);
 };
